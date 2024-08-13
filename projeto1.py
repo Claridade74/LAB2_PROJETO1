b96de902d6e0381dc3cb96df2431beed01bc8183
Clarissa Treptow <claritreptow.est@gmail.com>
2024-08-12 22:26:57 -0300

    Cria uma função que sorteia três números aleatórios e calcula a média deles, depois retorna a media e os números sorteados

--- projeto1.py
+++ projeto1.py
@@ -4,3 +4,13 @@ def cumprimento(texto):
 nome = "Clarissa Treptow"
 resultado = cumprimento(nome)
 resultado
+
+import random
+
+def media_numeros_aleatorios():
+    numeros = [random.randint(0, 1000) for i in range(3)]
+    media = sum(numeros) / len(numeros)
+    return media, numeros
+
+media, numeros_sorteados = media_numeros_aleatorios()
+media, numeros_sorteados
